e472fb71ebf0911c1b7482d57890aced925c5d42
Philipp Lensch <p.lensch@lesh.eu>
2020-10-08 20:39:38 +0200

    [fixed/added] (tempomap.c) corrected arithmetic mean, added 'lazy' image loading

--- tempomap.c
+++ tempomap.c
@@ -33,19 +33,32 @@ int main (int argc, char *argv[]) {
         return 1;
     }
 
+    // find unique pixel values to only load needed images from image sequence
+    unsigned char gray = 0;
+    unsigned char *p = map;
+    int unique[256] = {0};
+    for (unsigned int i = 0; i < (cx * cy * cn); i += cn) {
+        gray = (*(p+i) + *(p+i+1) + *(p+i+2)) / 3;
+        if (unique[gray] == 0) {
+            unique[gray] = 1;
+        }
+    }
+
     // load images
     int x,y,n;
     char imgpath[13];
     for (unsigned int i = 0; i < 256; i++) {
-        sprintf(imgpath, "%s/%03d.bmp",argv[2], i);
-        imgs[i] = stbi_load(imgpath, &x, &y, &n, 0);
-        if (imgs[i] == 0) {
-            fprintf(stderr, "Could not open image %s\n", imgpath);
-            return 1;
-        }
-        if ((n != cn) || (y != cy) || (x != cx)) {
-            fprintf(stderr, "Image format does not match map (width, height, channels): %s\n", imgpath);
-            return 1;
+        if (unique[i] != 0) {
+            sprintf(imgpath, "%s/%03d.bmp",argv[2], i);
+            imgs[i] = stbi_load(imgpath, &x, &y, &n, 0);
+            if (imgs[i] == 0) {
+                fprintf(stderr, "Could not open image %s\n", imgpath);
+                return 1;
+            }
+            if ((n != cn) || (y != cy) || (x != cx)) {
+                fprintf(stderr, "Image format does not match map (width, height, channels): %s\n", imgpath);
+                return 1;
+            }
         }
     }
 
@@ -53,8 +66,8 @@ int main (int argc, char *argv[]) {
     for (int i = 0; i < atoi(argv[3]); i++) {
         unsigned char gray = 0;
         unsigned char *p = map;
-        for (unsigned int i = 0; i < (cx*cy*cn); i += cn) {
-            gray = (*(p+i) * *(p+i+1) * *(p+i+2)) / 3;
+        for (unsigned int i = 0; i < (cx * cy * cn); i += cn) {
+            gray = (*(p+i) + *(p+i+1) + *(p+i+2)) / 3;
             *(p+i)   = *(imgs[gray]+i);
             *(p+i+1) = *(imgs[gray]+i+1);
             *(p+i+2) = *(imgs[gray]+i+2);
