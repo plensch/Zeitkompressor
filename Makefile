00da9fb983278299b334c611813fb990b5c6625f
Philipp Lensch <p.lensch@lesh.eu>
2020-10-09 21:08:48 +0200

    [added] (tempomap.c) rudimentary sequence output

--- Makefile
+++ Makefile
@@ -2,6 +2,7 @@ CC = gcc
 WINCC = x86_64-w64-mingw32-gcc
 OBJECTS = tempomap.c stb_image.h stb_image_write.h
 SAFE = -Wall -Wextra -fsanitize=address
+CHECK = -fsyntax-only -Wall -Wextra
 FAST = -O3 -static -s
 LIBS = -lm
 NAME = tempomap
@@ -9,6 +10,8 @@ NAME = tempomap
 fast: $(OBJECTS)
 	$(CC) $(FAST) -o $(NAME) $(OBJECTS) $(LIBS)
 safe: $(OBJECTS)
+	$(CC) $(SAFE) -o $(NAME) $(OBJECTS) $(LIBS)
+check:
 	$(CC) $(SAFE) -o $(NAME) $(OBJECTS) $(LIBS)
 windows: $(OBJECTS)
 	$(WINCC) $(FAST) -o $(NAME).exe $(OBJECTS) $(LIBS)
--- tempomap.c
+++ tempomap.c
@@ -10,6 +10,7 @@
  * add invert argument
  * remove assumptions
  * lazy load in rgb-mode too
+ * fix double-free in sequence mode
  *
  * ASSUMPTIONS:
  * three channel bitmap images (RGB)
@@ -29,11 +30,11 @@ void tm_unique (unsigned char *map, int *unique, int x, int y, int n) {
 }
 
 /* load image sequence */
-int tm_load (unsigned char **imgs, char *imgs_path, int *unique, int rgb, int cx, int cy, int cn) {
+int tm_load (unsigned char **imgs, char *imgs_path, int *unique, int rgb, int cx, int cy, int cn, int all) {
     int x,y,n;
     char imgpath[256]; // TODO make dynamic
     for (int i = 0; i < 256; i++) {
-        if (rgb ? 1 : unique[i]) { 
+        if (all || (rgb ? 1 : unique[i])) { 
             sprintf(imgpath, "%s%03d.bmp",imgs_path, i);
             imgs[i] = stbi_load(imgpath, &x, &y, &n, 0);
             if (imgs[i] == 0) {
@@ -50,26 +51,36 @@ int tm_load (unsigned char **imgs, char *imgs_path, int *unique, int rgb, int cx
 }
 
 /* tmap: map image sequence to canvas */
-void tm_map (unsigned char *map, unsigned char **imgs, int iterations, int rgb, int x, int y, int n) {
+void tm_map (unsigned char *map, unsigned char *canvas, unsigned char **imgs, int iterations,\
+             int rgb, int x, int y, int n) {
     unsigned char gray = 0;
     unsigned char r = 0;
     unsigned char g = 0;
     unsigned char b = 0;
+
+    unsigned char *m;
+    unsigned char *c;
     for (int i = 0; i < iterations; i++) {
-        unsigned char *p = map;
+        if (i < 1) {
+            m = map;
+            c = canvas;
+        } else {
+            m = canvas;
+            c = canvas;
+        }
         for (int i = 0; i < (x * y * n); i += n) {
             if (rgb) {
-                r = *(p+i);
-                g = *(p+i+1);
-                b = *(p+i+2);
-                *(p+i)   = *(imgs[r]+i);
-                *(p+i+1) = *(imgs[g]+i+1);
-                *(p+i+2) = *(imgs[b]+i+2);
+                r = *(m+i);
+                g = *(m+i+1);
+                b = *(m+i+2);
+                *(c+i)   = *(imgs[r]+i);
+                *(c+i+1) = *(imgs[g]+i+1);
+                *(c+i+2) = *(imgs[b]+i+2);
             } else {
-                gray = (*(p+i) + *(p+i+1) + *(p+i+2)) / 3;
-                *(p+i)   = *(imgs[gray]+i);
-                *(p+i+1) = *(imgs[gray]+i+1);
-                *(p+i+2) = *(imgs[gray]+i+2);
+                gray = (*(m+i) + *(m+i+1) + *(m+i+2)) / 3;
+                *(c+i)   = *(imgs[gray]+i);
+                *(c+i+1) = *(imgs[gray]+i+1);
+                *(c+i+2) = *(imgs[gray]+i+2);
             }
         }
     }
@@ -77,9 +88,11 @@ void tm_map (unsigned char *map, unsigned char **imgs, int iterations, int rgb, 
 
 int main (int argc, char *argv[]) {
     if (argc < 6) {
-        printf("Usage: %s [map] [image sequence folder] [iterations] [rgb/grayscale] [out]\n"
+        printf("Usage: %s [map] [image sequence folder] [iterations] [rgb/grayscale] [sequence/static] [out]\n"
                 "Images in the sequence should be labeled: 000.bmp, 001.bmp ... 255.bmp\n"
-                "rgb = 1, grayscale = 0\n", argv[0]);
+                "(rgb = 1, grayscale = 0)\n"
+                "(sequence = 1, static = 0)\n"
+                "When sequence = 1 then [out] should be a directory\n", argv[0]); 
         return 1;
     }
 
@@ -88,7 +101,8 @@ int main (int argc, char *argv[]) {
     char *imgs_path = argv[2];
     int iterations = atoi(argv[3]);
     int rgb = atoi(argv[4]);
-    char *out_path = argv[5];
+    int sequence = atoi(argv[5]);
+    char *out_path = argv[6];
 
     // load map
     int cx,cy,cn;
@@ -100,20 +114,32 @@ int main (int argc, char *argv[]) {
 
     // find which images are needed
     int unique[256] = {0};
-    tm_unique(map, unique, cx, cy, cn);
+    if (!sequence) {
+        tm_unique(map, unique, cx, cy, cn);
+    }
 
     // load images
     unsigned char *imgs[256] = {0};
-    if (tm_load(imgs, imgs_path, unique, rgb, cx, cy, cn)) {
+    if (tm_load(imgs, imgs_path, unique, rgb, cx, cy, cn, sequence)) {
         stbi_image_free(map);
         return 1; // passthrough error
     }
 
-    // map image sequence to canvas
-    tm_map(map, imgs, iterations, rgb, cx, cy, cn);
-
-    // save canvas
-    stbi_write_bmp(out_path, cx, cy, cn, map);
+    unsigned char *canvas = malloc(cx * cy * cn);
+    char outpath[256]; // TODO make dynamic
+    if (!sequence) {
+        // map image sequence to canvas
+        tm_map(map, canvas, imgs, iterations, rgb, cx, cy, cn);
+        // save canvas
+        stbi_write_bmp(out_path, cx, cy, cn, canvas);
+    } else {
+        for (int i = 0; i < 256; i++) {
+            map = imgs[i];
+            tm_map(map, canvas, imgs, iterations, rgb, cx, cy, cn);
+            sprintf(outpath, "%s%03d.bmp",out_path, i);
+            stbi_write_bmp(outpath, cx, cy, cn, canvas);
+        }
+    }
 
     // free images
     stbi_image_free(map);
